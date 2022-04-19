5ad40d35d4b3d2ab62632466ebf0c544d49fabad
Petra Christina <p.sedlaczek@drei.at>
2022-04-19 06:10:14 +0200

    Event Controller

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -3,34 +3,52 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Request;
 use Doctrine\Persistence\ManagerRegistry;
 use App\Form\EventType;
 use App\Entity\Event;
 
 class EventController extends AbstractController
 {
-  #[Route('/', name: 'events')]
+  #[Route("/", name: "events")]
   public function index(ManagerRegistry $doctrine): Response
   {
     $events = $doctrine->getRepository(Event::class)->findAll();
-    return $this->render('event/index.html.twig',
-      ['events' => $events]
+    return $this->render("event/index.html.twig",
+      ["events" => $events]
     );
   }
 
-  #[Route('/details/{id}', name: 'event_view')]
-  public function details(ManagerRegistry $doctrine, $id): Response
+  #[Route("/admin", name: "event_admin")]
+  public function admin(ManagerRegistry $doctrine): Response
+  {
+    $events = $doctrine->getRepository(Event::class)->findAll();
+    return $this->render("event/admin.html.twig",
+      ["events" => $events]
+    );
+  }
+
+  #[Route("/filter/{type}", name: "event_filter")]
+  public function filter(ManagerRegistry $doctrine, $type): Response
+  {
+    $events = $doctrine->getRepository(Event::class)->findBy(['type' => $type]);
+    return $this->render("event/filter.html.twig", 
+      ["events" => $events]
+    );
+  }
+
+  #[Route("/event/{id}", name: "event_view")]
+  public function view(ManagerRegistry $doctrine, $id): Response
   {
     $event = $doctrine->getRepository(Event::class)->find($id);
-    return $this->render('event/view.html.twig', 
-      ['event' => $event]
+    return $this->render("event/view.html.twig", 
+      ["event" => $event]
     );
   }
 
-  #[Route('/add', name: 'event_add')]
+  #[Route("/add", name: "event_add")]
   public function create(Request $request, ManagerRegistry $doctrine): Response
   {
     $event = new Event();
@@ -38,21 +56,19 @@ class EventController extends AbstractController
     $form->handleRequest($request);
 
     if ($form->isSubmitted() && $form->isValid()) {
-      $now = new \DateTime('now');
-      $event = $form->getData();
-      $event->setCreateDate($now);
       $em = $doctrine->getManager();
+      $event = $form->getData();
       $em->persist($event);
       $em->flush();
-      $this->addFlash('Success', 'Event added');
-      return $this->redirectToRoute('events');
+      $this->addFlash("success", "Event added");
+      return $this->redirectToRoute("event_admin");
     }
-    return $this->render('event/create.html.twig', 
-      ['form' => $form->createView()]
+    return $this->render("event/add.html.twig", 
+      ["form" => $form->createView()]
     );
   }
 
-  #[Route('/edit/{id}', name: 'event_edit')]
+  #[Route("/edit/{id}", name: "event_edit")]
   public function edit(Request $request, ManagerRegistry $doctrine, $id): Response
   {
     $event = $doctrine->getRepository(Event::class)->find($id);
@@ -60,28 +76,35 @@ class EventController extends AbstractController
     $form->handleRequest($request);
 
     if ($form->isSubmitted() && $form->isValid()) {
-      $now = new \DateTime('now');
-      $event = $form->getData();
-      $event->setCreateDate($now);
       $em = $doctrine->getManager();
+      $event = $form->getData();
       $em->persist($event);
       $em->flush();
-      $this->addFlash('Success', 'Event updated');
-      return $this->redirectToRoute('events');
+      $this->addFlash("success", "Event updated");
+      return $this->redirectToRoute("event_admin");
     }
-    return $this->render('event/edit.html.twig', 
-      ['form' => $form->createView()]
+    return $this->render("event/edit.html.twig", 
+      ["form" => $form->createView()]
     );
   }
 
-  #[Route('/delete/{id}', name: 'event_delete')]
-  public function delete(ManagerRegistry $doctrine, $id)
+  #[Route("/delete/{id}", name: "event_delete")]
+  public function delete(ManagerRegistry $doctrine, $id): Response
+  {
+    $event = $doctrine->getRepository(Event::class)->find($id);
+    return $this->render("event/delete.html.twig", 
+      ["event" => $event]
+    );
+  }
+
+  #[Route("/remove/{id}", name: "event_remove")]
+  public function remove(ManagerRegistry $doctrine, $id)
   {
     $em = $doctrine->getManager();
     $event = $em->getRepository(Event::class)->find($id);
     $em->remove($event);
     $em->flush();
-    $this->addFlash('Success', 'Event removed');
-    return $this->redirectToRoute('events');
+    $this->addFlash("success", "Event deleted");
+    return $this->redirectToRoute("event_admin");
   }
 }
