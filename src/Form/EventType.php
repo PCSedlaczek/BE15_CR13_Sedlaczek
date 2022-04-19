fe0c79411561332792aad1743ff895e9e5395639
Petra Christina <p.sedlaczek@drei.at>
2022-04-19 06:10:03 +0200

    EventType Form

--- src/Form/EventType.php
+++ src/Form/EventType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
@@ -20,40 +23,138 @@ class EventType extends AbstractType
   public function buildForm(FormBuilderInterface $builder, array $options): void
   {
     $builder
-      ->add('name', TextType::class, [
-        'attr' => ['class' => 'form-control mb-5']
+      ->add("title", TextType::class, [
+        "label" => "Event title",
+        "required" => true,
+        "attr" => [
+          "class" => "form-control mb-1"
+        ]
       ])
-      ->add('type', TextType::class, [
-        'attr' => ['class' => 'form-control mb-5']
+      ->add("subtitle", TextType::class, [
+        "label" => "Event subtitle",
+        "required" => false,
+        "attr" => [
+          "class" => "form-control mb-1"
+        ]
       ])
-      ->add('description', TextType::class, [
-        'attr' => ['class' => 'form-control mb-5']
+      ->add("type", TextType::class, [
+        "label" => "Event type",
+        "required" => false,
+        "attr" => [
+          "class" => "form-control mb-1"
+        ]
       ])
-      ->add('date', DateType::class, [
-        'attr' => ['class' => 'mb-5']
+      ->add("location", EntityType::class, [
+        "class" => Location::class,
+        "choice_label" => "name",
+        "label" => "Location: ",
+        "placeholder" => "Choose location",
+        "attr" => [
+          "class" => "form-control mb-1"
+        ]
       ])
-      ->add('start', TimeType::class, [
-        'attr' => ['class' => 'mb-5']
+      ->add("organizer", EntityType::class, [
+        "class" => Organizer::class,
+        "choice_label" => "name",
+        "label" => "Organizer: ",
+        "placeholder" => "Choose organizer",
+        "attr" => [
+          "class" => "form-control mb-1"
+        ]
       ])
-      ->add('end', TimeType::class, [
-        'attr' => ['class' => 'mb-5']
+      ->add("date", DateType::class, [
+        "label" => "Event date",
+        "placeholder" => [
+          'month' => 'Month', 
+          'day' => 'Day',
+          'year' => 'Year',
+        ],
+        "required" => true,
+        "attr" => [
+          "class" => "mb-1"
+        ]
       ])
-      ->add('location', EntityType::class, [
-        'class' => Location::class,
-        'choice_label' => 'name',
-      ])   
-      ->add('organizer', EntityType::class, [
-        'class' => Organizer::class,
-        'choice_label' => 'name',
-      ])   
-      ->add('save', SubmitType::class, [
-        'choice_label' => 'Create event',
-        'attr' => ['class' => 'btn-primary mb-5']
+      ->add("start", TimeType::class, [
+        "label" => "Start time",
+        "required" => false,
+        "placeholder" => [
+          "hour" => "Hour", 
+          "minute" => "Minute"
+        ],
+        "attr" => [
+          "class" => "mb-1"
+        ]
+      ])
+      ->add("end", TimeType::class, [
+        "label" => "End time",
+        "required" => false,
+        "placeholder" => [
+          "hour" => "Hour", 
+          "minute" => "Minute"
+        ],
+        "attr" => [
+          "class" => "mb-1"
+        ]
+      ])
+      ->add("lead", TextType::class, [
+        "label" => "Lead paragraph",
+        "required" => false,
+        "attr" => [
+          "class" => "form-control mb-1"
+        ]
+      ])
+      ->add("description", TextareaType::class, [
+        "label" => "Event description",
+        "required" => false,
+        "attr" => [
+          "class" => "form-control mb-1",
+          "style" => "height: 250px"
+        ]
+      ])
+      ->add("capacity", TextType::class, [
+        "label" => "Event capacity",
+        "required" => false,
+        "attr" => [
+          "class" => "form-control mb-1",
+          "type" => "number"
+        ]
+      ])
+      ->add("price", MoneyType::class, [
+        "label" => "Event price",
+        "required" => false,
+        "html5" => true,
+        "divisor" => 100,
+        "data" => 0.00,
+        "attr" => [
+          "class" => "form-control mb-1"
+        ]
+      ])
+      ->add("img", TextType::class, [
+        "label" => "Event image",
+        "required" => false,
+        "attr" => [
+          "class" => "form-control mb-1",
+          "type" => "url"
+        ]
+      ])
+      ->add("url", TextType::class, [
+        "label" => "Event URL",
+        "required" => true,
+        "attr" => [
+          "class" => "form-control mb-1",
+          "type" => "url"
+        ]
+      ])
+      ->add("save", SubmitType::class, [
+        "label" => "Save",
+        "attr" => [
+          "class" => "btn btn-success my-1"
+        ]
       ]);
   }
 
   public function configureOptions(OptionsResolver $resolver): void
   {
-    $resolver->setDefaults(['data_class' => Event::class]);
+    $resolver->setDefaults(["data_class" => Event::class]);
   }
 }
